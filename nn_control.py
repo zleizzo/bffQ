f74e63258d17433848a88733c990e8f57d5ff32c
Zach Izzo <zizzo@stanford.edu>
2020-04-26 00:41:51 -0700

    Fixed trajectory to use e-greedy

--- nn_control.py
+++ nn_control.py
@@ -86,8 +86,7 @@ def compute_j(cur_s, cur_a, nxt_s, Q, e = 0.1):
     cur_s = map_to_input(cur_s)
     nxt_s = map_to_input(nxt_s)
     
-    nxt_a = e_greedy(Q(nxt_s), e)
-    return rwd + g * Q(nxt_s)[nxt_a] - Q(cur_s)[cur_a]
+    return rwd + g * torch.max(Q(nxt_s)) - Q(cur_s)[cur_a]
 
 
 def compute_grad_j(cur_s, cur_a, nxt_s, Q, e = 0.1):
@@ -112,7 +111,7 @@ def UB(T, learning_rate, batch_size, e = 0.1, Q = Net(), trueQgraph = None):
         grads = [torch.zeros(w.shape) for w in Q.parameters()]
         for i in range(batch_size):
             cur_s = nxt_s
-            cur_a = policy(cur_s)
+            cur_a = e_greedy(Q(cur_s), e)
             nxt_s = transition(cur_s, cur_a)
             new_s = transition(cur_s, cur_a)
             
@@ -148,7 +147,7 @@ def DS(T, learning_rate, batch_size, e = 0.1, Q = Net(), trueQgraph = None):
         grads = [torch.zeros(w.shape) for w in Q.parameters()]
         for i in range(batch_size):
             cur_s = nxt_s
-            cur_a = policy(cur_s)
+            cur_a = e_greedy(Q(cur_s), e)
             nxt_s = transition(cur_s, cur_a)
             new_s = nxt_s
             
@@ -178,10 +177,10 @@ def BFF(T, learning_rate, batch_size, e = 0.1, Q = Net(), trueQgraph = None):
     
     print('Starting BFF SGD...')
     cur_s = np.random.rand() * 2 * np.pi
-    cur_a = policy(cur_s)
+    cur_a = e_greedy(cur_s, e)
     
     nxt_s = transition(cur_s, cur_a)
-    nxt_a = policy(nxt_s)
+    nxt_a = e_greedy(nxt_s, e)
     
     ftr_s = transition(nxt_s, nxt_a)
     for k in range(int(T / batch_size)):
@@ -193,7 +192,7 @@ def BFF(T, learning_rate, batch_size, e = 0.1, Q = Net(), trueQgraph = None):
             cur_a = nxt_a
             
             nxt_s = ftr_s
-            nxt_a = policy(nxt_s)
+            nxt_a = e_greedy(nxt_s, e)
             
             ftr_s = transition(nxt_s, nxt_a)
             
@@ -216,37 +215,6 @@ def BFF(T, learning_rate, batch_size, e = 0.1, Q = Net(), trueQgraph = None):
     return Q, errors
 
 
-def monte_carlo(s, a, trueQ, e = 0.1, tol = 0.001, reps = 100):
-    
-    # T is defined so that the total reward incurred from time T to infinity is
-    # at most tol.
-    R_max = 2
-    T = int(np.log((1 - g) * tol / R_max) / np.log(g)) + 1
-    
-    total = 0
-    for r in range(reps):
-        s_cur = s
-        a_cur = a
-        discount = 1
-        for t in range(1, T):
-            s_cur = transition(s_cur, a_cur)
-            total += reward(s_cur) * discount
-            a_cur = e_greedy(trueQ(map_to_input(s_cur)), e)
-            discount *= g
-    empirical_avg = total / reps
-    return empirical_avg
-
-
-def MC(trueQ, e = 0.1, tol = 0.001, reps = 1000, divisions = 50):
-    
-    Q = np.zeros((divisions, 2))
-    for i, s in zip(range(divisions), np.linspace(0, 2 * np.pi, divisions)):
-        for a in range(2):
-            print(f'Computing Q({s}, {a})...')
-            Q[i, a] = monte_carlo(s, a, trueQ, e, tol, reps)
-    return Q
-
-
 random.seed(0)
 
 T             = 1000000
@@ -263,9 +231,7 @@ trueQgraph = trueQ(z).detach()
 Q_UB,  e_UB  = UB(T, learning_rate, batch_size, e, Net(), trueQgraph)
 Q_DS,  e_DS  = DS(T, learning_rate, batch_size, e, Net(), trueQgraph)
 Q_BFF, e_BFF = BFF(T, learning_rate, batch_size, e, Net(), trueQgraph)
-#Q_MC         = MC(trueQ, e)
 
-#mc  = Q_MC
 ub  = Q_UB(z).detach()
 ds  = Q_DS(z).detach()
 bff = Q_BFF(z).detach()
@@ -274,7 +240,6 @@ true = trueQ(z).detach()
 plt.figure()
 plt.subplot(1,2,1)
 plt.plot(x, true[:, 0], label='true', color='m')
-#plt.plot(x, mc[:, 0], label='mc', color='c')
 plt.plot(x, ub[:, 0], label='ub', color='b')
 plt.plot(x, ds[:, 0], label='ds', color='r')
 plt.plot(x, bff[:, 0], label='bff', color='g')
@@ -283,14 +248,12 @@ plt.legend()
 
 plt.subplot(1,2,2)
 plt.plot(x, true[:, 1], label='true', color='m')
-#plt.plot(x, mc[:, 1], label='mc', color='c')
 plt.plot(x, ub[:, 1], label='ub', color='b')
 plt.plot(x, ds[:, 1], label='ds', color='r')
 plt.plot(x, bff[:, 1], label='bff', color='g')
 plt.title('Q, action 1')
 plt.legend()
 plt.savefig('plots/nn_q_control.png')
-
 
 
 rel_e_UB  = [err / e_UB[0]  for err in e_UB]
