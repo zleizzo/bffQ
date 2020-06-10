4e343d67eeb27a75e875a34115f253b2729e600d
Zach Izzo <zizzo@stanford.edu>
2020-06-09 20:34:56 -0700

    scripts for testing all methods

--- tabular_control_helper.py
+++ tabular_control_helper.py
@@ -1,17 +1,31 @@
-filename = 'find_trueQ_tab_ctrl.sh'
-job_name = 'trueQ_tc'
+methods = ['ub', 'ds', 'bff']
+ns = range(5)
 
-f = open(filename, 'w')
-f.write('#!/bin/bash\n')
-f.write('#\n')
-f.write(f'#SBATCH --job-name={job_name}\n')
-f.write('#\n')
-f.write('#SBATCH --time=10:00:00\n')
-f.write('#\n')
-f.write('#SBATCH --partition=jamesz,owners,normal\n')
-f.write('#SBATCH --ntasks=1\n')
-f.write('#SBATCH --cpus-per-task=1\n')
-f.write('#SBATCH --mem-per-cpu=8G\n')
-f.write('\n')
-f.write(f'srun python tabular_control.py')
-f.close()
+for method in methods:
+    for n in range(1, 6):
+        method_copy = method
+        
+        if method == 'bff':
+            method_copy = str(n) + 'bff'
+        print(method_copy)
+            
+        filename = f'tc_{method_copy}.sh'
+        job_name = f'tc_{method_copy}'
+        
+        f = open(filename, 'w')
+        f.write('#!/bin/bash\n')
+        f.write('#\n')
+        f.write(f'#SBATCH --job-name={job_name}\n')
+        f.write('#\n')
+        f.write('#SBATCH --time=10:00:00\n')
+        f.write('#\n')
+        f.write('#SBATCH --partition=jamesz,owners,normal\n')
+        f.write('#SBATCH --ntasks=1\n')
+        f.write('#SBATCH --cpus-per-task=1\n')
+        f.write('#SBATCH --mem-per-cpu=8G\n')
+        f.write('\n')
+        if method == 'bff':
+            f.write(f'srun python tabular_control.py {method} {n}')
+        else:
+            f.write(f'srun python tabular_control.py {method}')
+        f.close()
